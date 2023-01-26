3be4402e8ac95147efee33c7997060e80184e99c
Kramer Brown <brownkramer63@gmail.com>
2023-01-26 12:33:34 -0600

    API review

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/service/Implementation/ProductServiceImpl.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/Implementation/ProductServiceImpl.java
@@ -1,16 +1,30 @@
 package com.cydeo.lab08rest.service.Implementation;
 
 import com.cydeo.lab08rest.dto.ProductDTO;
+import com.cydeo.lab08rest.mapper.MapperUtil;
+import com.cydeo.lab08rest.repository.ProductRepository;
 import com.cydeo.lab08rest.service.ProductService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+    private final MapperUtil mapperUtil;
+
+    public ProductServiceImpl(ProductRepository productRepository, MapperUtil mapperUtil) {
+        this.productRepository = productRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
     @Override
     public List<ProductDTO> retrieveProductList() {
-        return null;
+        return productRepository.findAll().stream()
+                .map(product->mapperUtil.convert(product, new ProductDTO())).collect(Collectors.toList());
     }
 
     @Override
